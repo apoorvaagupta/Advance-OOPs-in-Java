3d04a8d7917083a3b77821e32fb07806c436f35a
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-04-03 00:19:58 +0530

    added the code for driver program
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

--- src/main/java/me/apoorvaagupta/App.java
+++ src/main/java/me/apoorvaagupta/App.java
@@ -6,8 +6,37 @@ package me.apoorvaagupta;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        ConsoleInput<String> cis = new ConsoleInput<String>(String.class);
+        AdditionProcessor<String> aps = new AdditionProcessor<String>(String.class);
+        ReversalProcessor<String> rps = new ReversalProcessor<String>(String.class);
+        ConsoleOutput<String> cos = new ConsoleOutput<String>();
+
+        String s1 = cis.input();
+        String s2 = cis.input();
+
+        String sum = aps.process(s1, s2);
+        String rev = rps.process(s1);
+
+        cos.output(sum);
+        cos.output(rev);
+
+        ConsoleInput<Integer> cii = new ConsoleInput<Integer>(Integer.class);
+        AdditionProcessor<Integer> api = new AdditionProcessor<Integer>(Integer.class);
+        ReversalProcessor<Integer> rpi = new ReversalProcessor<Integer>(Integer.class);
+        ConsoleOutput<Integer> coi = new ConsoleOutput<Integer>();
+
+        Integer v1 = cii.input();
+        Integer v2 = cii.input();
+
+        Integer sum2 = api.process(v1, v2);
+        Integer rev2 = rpi.process(v1);
+
+        coi.output(sum2);
+        coi.output(rev2);
+
+
+
     }
 }
